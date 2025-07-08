47b8c14cbeeb404bc25c0636e5f58e425e78a6db
habib-676 <rahman.habibur2421k@gmail.com>
2025-07-09 02:49:38 +0600

    set a loader and toast before and after the plant adding

--- client/src/pages/Dashboard/Seller/AddPlant.jsx
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -2,12 +2,17 @@ import AddPlantForm from "../../../components/Form/AddPlantForm";
 import { imageUpload } from "../../../api/utils";
 import axios from "axios";
 import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
   const { user } = useAuth();
 
+  const [isUploading, setIsUploading] = useState(false);
+
   const handleFormSubmit = async (e) => {
     e.preventDefault();
+    setIsUploading(true);
     const form = e.target;
     const name = form?.name?.value;
     const category = form?.category?.value;
@@ -16,32 +21,44 @@ const AddPlant = () => {
     const quantity = form?.quantity?.value;
 
     // image upload
-    const image = form?.image?.files[0];
-    const imageUrl = await imageUpload(image);
+    try {
+      const image = form?.image?.files[0];
+      const imageUrl = await imageUpload(image);
 
-    const plantData = {
-      name,
-      category,
-      description,
-      price,
-      quantity,
-      image: imageUrl,
-      seller: {
-        name: user?.displayName,
-        email: user?.email,
-      },
-    };
+      const plantData = {
+        name,
+        category,
+        description,
+        price,
+        quantity,
+        image: imageUrl,
+        seller: {
+          name: user?.displayName,
+          email: user?.email,
+        },
+      };
 
-    const { data } = await axios.post(
-      `${import.meta.env.VITE_API_URL}/add-plant`,
-      plantData
-    );
-    console.log(data);
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/add-plant`,
+        plantData
+      );
+      console.log(data);
+
+      toast.success("Plant data added successfully");
+      form.reset();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsUploading(false);
+    }
   };
   return (
     <div>
       {/* Form */}
-      <AddPlantForm handleFormSubmit={handleFormSubmit} />
+      <AddPlantForm
+        handleFormSubmit={handleFormSubmit}
+        isUploading={isUploading}
+      />
     </div>
   );
 };
