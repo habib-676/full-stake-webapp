90df37e06c651558cb13a2d08337c42c47a1ca57
habib-676 <rahman.habibur2421k@gmail.com>
2025-07-12 22:52:03 +0600

    handle data with query

--- client/src/hooks/useRole.jsx
+++ client/src/hooks/useRole.jsx
@@ -1,26 +1,45 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
 
 const useRole = () => {
-  const { user } = useAuth();
-  const [role, setRole] = useState(null);
-  const [isRoleLoading, setIsRoleLoading] = useState(true);
+  const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
+  // const [role, setRole] = useState(null);
+  // const [isRoleLoading, setIsRoleLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      const { data } = await axiosSecure(
-        `${import.meta.env.VITE_API_URL}/users/role/${user?.email}`
-      );
-      setRole(data.role);
-      setIsRoleLoading(false);
-    };
-    fetchUserRole();
-  }, [user, axiosSecure]);
+  // handle using tanstack query :
+  const { data: role, isLoading: isRoleLoading } = useQuery({
+    queryKey: ["role", user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users/role/${user?.email}`);
+      return data;
+    },
+  });
+
+  // handling manually
+  // useEffect(() => {
+  //   const fetchUserRole = async () => {
+  //     try {
+  //       if (!user) return setIsRoleLoading(false);
+
+  //       const { data } = await axiosSecure(
+  //         `${import.meta.env.VITE_API_URL}/users/role/${user?.email}`
+  //       );
+  //       setRole(data.role);
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       setIsRoleLoading(false);
+  //     }
+  //   };
+  //   fetchUserRole();
+  // }, [user, axiosSecure]);
 
   console.log(role);
-  return [role, isRoleLoading];
+  return [role.role, isRoleLoading];
 };
 
 export default useRole;
